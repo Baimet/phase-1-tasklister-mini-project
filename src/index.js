5a3b10d753864838a939d9a7683ffd440dfce3dc
baimet <isaacbaimetyatich@gmail.com>
2024-07-15 12:25:36 +0300

    tasklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const taskDescription = document.getElementById(
+      "new-task-description"
+    ).value;
+
+    addTask(taskDescription);
+
+    form.reset();
+  });
+
+  function addTask(description) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = description;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      taskItem.remove();
+    });
+
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+  }
 });
